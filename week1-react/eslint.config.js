07dd61ac343e0fb19cf77788eaaec54b092f7ee5
eunjeong Jang <eunjeongJang@lotte.net>
2026-04-18 14:31:09 +0900

    fix: [week01/step-04] lint rules 설정(인덴트, 세미콜론, 따옴표 옵션), 에디터 codeActionsOnSave

---
+++ week1-react/eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-undef': 'warn',
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'semi': ['error', 'always'],
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+];
